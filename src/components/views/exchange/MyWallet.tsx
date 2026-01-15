97868fcb3c25d20e105afec17fd0e4e0f3a08930
PangHae <yds05074@gmail.com>
2026-01-16 04:53:50 +0900

    style(my wallet): change font size

--- src/components/views/exchange/MyWallet.tsx
+++ src/components/views/exchange/MyWallet.tsx
@@ -8,22 +8,22 @@ export const MyWallet = () => {
 				<CardTitle className="text-xl font-bold">내 지갑</CardTitle>
 			</CardHeader>
 			<CardContent className="flex flex-col gap-4 flex-1">
-				<ul className="flex flex-col gap-4 flex-1">
+				<ul className="flex flex-col gap-4 flex-1 text-xl font-medium">
 					<li className="flex justify-between items-center">
 						<span className="text-gray-700">KRW</span>
-						<span className="text-gray-700">₩ 1,200,000</span>
+						<span className="text-gray-700 font-semibold">₩ 1,200,000</span>
 					</li>
 					<li className="flex justify-between items-center">
 						<span className="text-gray-700">USD</span>
-						<span className="text-gray-700">$ 50,000</span>
+						<span className="text-gray-700 font-semibold">$ 50,000</span>
 					</li>
 					<li className="flex justify-between items-center">
 						<span className="text-gray-700">JPY</span>
-						<span className="text-gray-700">₩ 150,000</span>
+						<span className="text-gray-700 font-semibold">₩ 150,000</span>
 					</li>
 				</ul>
 				<Separator />
-				<div className="flex justify-between items-center">
+				<div className="flex justify-between items-center text-xl font-medium">
 					<span className="text-gray-700">총 보유 자산</span>
 					<span className="text-xl font-bold text-blue-600">₩ 3,000,000</span>
 				</div>
